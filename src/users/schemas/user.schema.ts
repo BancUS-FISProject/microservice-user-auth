63a78e968f3e0d6568a970ee85c00d6c25dae96b
rubense110 <rubenjj99@gmail.com>
2025-11-23 19:34:35 +0100

    fix:  fixed small error on auth module related to user schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
 
-export type UserDocument = User & Document;
+export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
